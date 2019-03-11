74678867adb4997c4744ba2f6114a94ee5972b47
Pozharov Roman <ru.roman.ps@gmail.com>
2019-03-11 05:56:58 +0400

    Правки

--- main.js
+++ main.js
@@ -1,24 +1,32 @@
+const keyEnter = 13;
+const keyESC = 27;
 var newElement = document.createElement('input');
+newElement.className = "input input__new";
 var input = document.querySelector(".input");
 var main = document.querySelector(".main");
-
-newElement.className = "input input__new";
+var inputNew = document.querySelector(".input input__new");
 
 input.addEventListener("dblclick", function(event) {
   event.preventDefault;
   if (input.value != '') {
     main.appendChild(newElement);
     newElement.value = input.value;
-    newElement.setAttribute("readonly", "readonly");
+    newElement.setAttribute("", "");
   }
 });
 
 input.addEventListener("keydown", function(event) {
-  if (event.keyCode === 13) {
+  if (event.keyCode === keyEnter) {
     if (input.value != '') {
       main.appendChild(newElement);
       newElement.value = input.value;
-      newElement.setAttribute("readonly", "readonly");
+      newElement.setAttribute("", "");
     }
   }
 });
+
+inputNew.addEventListener("keydown", function(event) {
+  if (event.keyCode === keyESC) {
+    main.removeChild(inputNew);
+  }
+})
