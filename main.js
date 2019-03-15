677dddfcd2c95f953bc39f1e603200b7a714a631
Pozharov Roman <ru.roman.ps@gmail.com>
2019-03-15 23:17:48 +0400

    Добавлена функция создания нового элемента

--- main.js
+++ main.js
@@ -2,9 +2,7 @@ const KEYCODE_ENTER = 13;
 var input = document.querySelector(".add");
 var active = document.querySelector(".active");
 
-// добавление элемента
-function addsItem(evt){
-  evt.preventDefault;
+function createElem() {
   if (input.value != '') {
     var newElement = document.createElement('div');
     newElement.className = "task";
@@ -15,28 +13,27 @@ function addsItem(evt){
   input.value = '';
 }
 
+// добавление по клику
+function addsItemByClick(evt){
+  evt.preventDefault;
+    createElem();
+}
+
+// добавление по клавише
+function addsItemByBtn(evt){
+  evt.preventDefault;
+  if (event.keyCode === KEYCODE_ENTER) {
+    createElem();
+  }
+}
+
 // удаление элемента
 function deleteItem(evt) {
   evt.preventDefault();
   var target = evt.target;
   target.remove();
-  console.log(target);
   target.removeEventListener("click", deleteItem);
 }
 
-input.addEventListener("dblclick", addsItem);
-
-input.addEventListener("keydown", function(event) {
-  event.preventDefault;
-  if (event.keyCode === KEYCODE_ENTER) {
-    if (input.value != '') {
-      var newElement = document.createElement('div');
-      newElement.className = "task";
-      newElement.addEventListener("click", deleteItem);
-      active.appendChild(newElement);
-      newElement.innerHTML = input.value;
-    }
-    console.log(newElement);
-    input.value = '';
-  }
-});
+input.addEventListener("dblclick", addsItemByClick);
+input.addEventListener("keydown", addsItemByBtn);
