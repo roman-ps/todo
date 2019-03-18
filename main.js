76fda49808f4fd56e6b41b20850d2110db5886ca
Pozharov Roman <ru.roman.ps@gmail.com>
2019-03-18 10:04:10 +0400

    Добавлен счетчик выполненных заданий

--- main.js
+++ main.js
@@ -5,14 +5,12 @@ var taskText = tmpl.content.querySelector(".task__main");
 var input = document.querySelector(".add");
 var active = document.querySelector(".active");
 var tasksAll = document.querySelector(".tasks__all span");
-var tasksActive = document.querySelector(".tasks__active span");
+var tasksComplete = document.querySelector(".tasks__complete span");
 
 // создание нового элемента
 function createElem() {
   if (input.value != '') {
     var newElement = task.cloneNode(true);
-    console.log(task);
-    console.log(newElement);
     newElement.addEventListener("click", deleteItem);
     active.appendChild(newElement);
     var taskTextNew = newElement.querySelector(".task__main");
@@ -44,7 +42,23 @@ function deleteItem(evt) {
     evt.currentTarget.remove();
     tasksAll.innerHTML = parseInt(tasksAll.innerHTML) - 1;
   }
+  if (evt.target === evt.currentTarget) {
+    if (evt.target.classList.contains("complete")) {
+      evt.target.classList.remove("complete");
+      evt.target.classList.remove("linethrough");
+      tasksComplete.innerHTML = parseInt(tasksComplete.innerHTML) - 1;
+      tasksAll.innerHTML = parseInt(tasksAll.innerHTML) + 1;
+    }
+    else {
+      evt.target.classList.add("complete");
+      evt.target.classList.add("linethrough");
+      tasksComplete.innerHTML = parseInt(tasksComplete.innerHTML) + 1;
+      tasksAll.innerHTML = parseInt(tasksAll.innerHTML) - 1;
+    }
+  }
 }
 
 input.addEventListener("dblclick", addsItemByClick);
 input.addEventListener("keydown", addsItemByBtn);
+
+//element.classList.contains(class);
